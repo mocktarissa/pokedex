d9b2f501ce22493e3e6c313645540579b509ddfe
Mocktar <mocktarissa@gmail.com>
2019-09-29 13:39:07 +0300

    change colors and added a second deck

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Pokemon from './Pokemon';
+import Pokedex from './Pokedex';
+import './Pokedex.css'
 
 function App() {
-  var temp=[
-    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-    {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-    {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-    {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-    {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-    {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-    {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-    {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-  ];
-  return (<div>
-    {temp.map(m=><Pokemon id={m.id} name={m.name} type={m.type} base_experience={m.base_experience}/>)}
-    
-  </div>
-    
-  );
+  return(<div> <h1> Pokegame</h1>
+    <h2>Deck 1</h2>
+    <Pokedex className="Pokedex"/>
+    <h2>Deck 2</h2>
+    <Pokedex className="Pokedex"/>
+  </div>)
 }
 
 export default App;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './pokemon.css'
 class Pokemon extends React.Component{
-    
+    static defaultProps={id: 4, name: 'Charmander', type: 'fire', base_experience: 62
+    }
     render(){
         var poke=this.props;
         var urla="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+poke.id+".png"
         return (<div className='pokemon'>
             <h1>{poke.name}</h1>
             <img src={urla} alt={poke.name}/>
-            <p>
-                <span>Type:{poke.type}</span>
-                <span>Base experience:{poke.base_experience}</span>
-            </p>
+            <div>
+                <p>Type:{poke.type}</p>
+                <p>Base experience:{poke.base_experience}</p>
+            </div>
 
         </div>);
     }
